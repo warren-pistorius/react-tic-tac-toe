fff0dc9ad0c972a0d7d93d7b9c3df840bff2ee73
Warren Pistorius <warren@sleevesup.com.au>
2025-02-26 12:58:54 +0800

    Clickable buttons. Multi-dimensional array with updating without modifying existing gameboard.

--- src/Components/GameBoard/GameBoard.jsx
+++ src/Components/GameBoard/GameBoard.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,29 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function onCellClick(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      //NOTE: Don't change the previous gameboard. Create a new one.
+      const newGameBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      newGameBoard[rowIndex][colIndex] = "X"; // or "O" based on the current player
+      return newGameBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <span key={colIndex} className="game-cell">
-                <button>{playerSymbol}</button>
+                <button onClick={() => onCellClick(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </span>
             ))}
           </ol>
